f32418fa24df65ec2c9544bd51787f9689b1fb9d
valdofal <v.dofal@gmail.com>
2020-11-22 16:45:56 +0100

    last commit

--- server/myFtpServer.js
+++ server/myFtpServer.js
@@ -5,9 +5,11 @@ const fs = require('fs')
 const rawusers = JSON.parse(fs.readFileSync('users.json','utf-8'))
 const usernames = fs.readFileSync('users.json','utf-8').match(/(?<=userName":")\w+/gmi)
 const server = net.createServer((socket) => {
+
   let currentUser = ''
   let isUser = false
   let isLogged = false
+
   socket.on('data', (data) => {
     console.log(data.toString())
     const [directive, parameter] = data.toString().split(' ')
@@ -25,6 +27,7 @@ const server = net.createServer((socket) => {
           isLogged = false
         }
         break;
+
       case 'PASS':
         if(currentUser == 'valentin' && rawusers[0]['password'] == parameter){
           isLogged = true
@@ -38,6 +41,7 @@ const server = net.createServer((socket) => {
           socket.write(`incorrect password, please enter a valid password for username : ${currentUser}`)
         }
         break;
+
         case 'LIST':
           directorycontent = fs.readdirSync('./','utf-8')
           socket.write(`The current folder (${process.cwd()}) contains : `)
@@ -46,51 +50,74 @@ const server = net.createServer((socket) => {
            
           }
           break;
+
         case 'CWD':
-            process.chdir(parameter, (err) => {
-              if(err) socket.write(`The directory you tried to access doesn't exist, please enter a valid directory`)
-              else socket.write(`You changed the working directory to : ${process.cwd()}`)
-            })
-          
+          if(isLogged == false){
+            socket.write('You must be logged in to have access to this command')
+          }
+          else {
+            try{
+            process.chdir(parameter)
+            socket.write(`You changed the working directory to : ${process.cwd()}`)
+           }
+            catch(err){
+            socket.write("The directory you tried to access doesn't exist, please use the LIST command to see directories")
+          }
+          }
           break;
+
         case 'RETR':
+          if(isLogged == false){
+            socket.write('You must be logged in to have access to this command')
+          }
+          else {
             try {
-              fs.copyFileSync(parameter,'../client/copytest(copied).txt')
+              fs.copyFileSync(parameter,'../client/'+parameter)
               socket.write('File was succesfully copied')
             }
               catch (err){
                  socket.write("File could not be found or doesn't exist")
             }
+          }
           break;
+
         case 'STOR':
-          try {
-            fs.copyFileSync('../client/'+parameter,'copytest2(copied).txt')
-            socket.write('File was succesfully copied')
+          if(isLogged == false){
+            socket.write('You must be logged in to have access to this command')
           }
-            catch (err){
-               socket.write("File could not be found or doesn't exist")
-          }
-          
+          else {
+            try {
+              fs.copyFileSync('../client/'+parameter,parameter)
+              socket.write('File was succesfully copied')
+            }
+              catch (err){
+                socket.write("File could not be found or doesn't exist")
+            }
+          }          
           break;
+
         case 'PWD':
-          socket.write(`The current server directory is : ${process.cwd()}`)
-          
+          if(isLogged == false){
+            socket.write('You must be logged in to have access to this command')
+          }
+          else {
+            socket.write(`The current server directory is : ${process.cwd()}`)
+          }          
+          break;
+      case'QUIT': 
+          console.log('Client has lost connection to the server')
           break;
       case 'HELP':
           socket.write(fs.readFileSync('help.txt', (err)=>{
             if (err) socket.write('Help is not accessible from this directory, change directory to server to use HELP')
           }))
         break;
+
       default:
         socket.write('You didnt enter a valid command, use <HELP> to see different commands')
         break;
-
-
-
     }
 })
-})
-
-server.listen(5000, () => {
-  console.log('Server started at port 5000')
+}).listen(process.argv[2], () => {
+  console.log(`Server started at port ${process.argv[2]}`)
 })
